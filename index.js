20bc76f619b53b2fac8b02c40b068a4ef0df7a45
madphoenixc <51910325+madphoenixc@users.noreply.github.com>
2021-08-29 15:38:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fastify = require('fastify')({
   // database connection
   const mongoose = require('mongoose');
 
-//   fastify.register(require('fastify-cors'), { origin: true })
+  fastify.register(require('fastify-cors'), { origin: true })
   mongoose.connect('mongodb+srv://gokul:gokul@cluster0.xo1l9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(() => console.log("Mongo is ready !"))
 .catch(err=> console.log(err))
